5a7ed9df095d3e2205855d7851c7d63c76af81d0
xavier sanchez <holaxave@gmail.com>
2020-11-27 17:16:28 +0100

    last cocktail

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,6 @@
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients
+  has_one_attached :photo
   validates :name, uniqueness: true, presence: true
 end
